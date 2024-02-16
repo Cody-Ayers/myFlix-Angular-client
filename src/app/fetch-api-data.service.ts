9dd36869af0b19b27fca0b5d1a4d025817d3fd19
Cody-Ayers <122771685+Cody-Ayers@users.noreply.github.com>
2024-02-16 16:18:39 -0700

    added user registration and user login forms

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -1,32 +1,27 @@
 import { Injectable } from '@angular/core';
-import { catchError } from 'rxjs/internal/operators';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { map } from 'rxjs/operators';
+
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'YOUR_HOSTED_API_URL_HERE/';
+const apiUrl = 'https://codys-flix-0b23a40a1d0d.herokuapp.com/';
 @Injectable({
   providedIn: 'root'
 })
-export class UserRegistrationService {
+export class FetchApiDataService {
   // Inject the HttpClient module to the constructor params
  // This will provide HttpClient to the entire class, making it available via this.http
   constructor(private http: HttpClient) {
   }
  // Making the api call for the user registration endpoint
-  public userRegistration(userDetails: any): Observable<any> {
-    console.log(userDetails);
-    return this.http.post(apiUrl + 'users', userDetails).pipe(
-    catchError(this.handleError)
-    );
-  }
+ public userRegistration(userDetails: any): Observable<any> {
+  console.log(userDetails);
+  return this.http.post(apiUrl + 'users', userDetails)
+}
 
   public userLogin(userDetails: any): Observable<any> {
     console.log(userDetails);
-    return this.http.post(apiUrl + 'login', userDetails).pipe(
-    catchError(this.handleError)
-    );
+    return this.http.post(apiUrl + 'login', userDetails)
   }
 
 
@@ -35,10 +30,7 @@ export class UserRegistrationService {
     return this.http.get(apiUrl + 'movies', {headers: new HttpHeaders(
       {
         Authorization: 'Bearer ' + token,
-      })}).pipe(
-      map(this.extractResponseData),
-      catchError(this.handleError)
-    );
+      })})
   }
 
   getOneMovies(title: string): Observable<any> {
@@ -48,10 +40,7 @@ export class UserRegistrationService {
         {
           Authorization: 'Bearer ' + token,
         })
-    }).pipe(
-      map(this.extractResponseData),
-      catchError(this.handleError)
-    );
+    })
   }
 
 
@@ -60,10 +49,7 @@ export class UserRegistrationService {
     return this.http.get(apiUrl + 'movies/directors/:Name', {headers: new HttpHeaders(
       {
         Authorization: 'Bearer ' + token,
-      })}).pipe(
-      map(this.extractResponseData),
-      catchError(this.handleError)
-    );
+      })})
   }
 
   getGenre(): Observable<any> {
@@ -71,10 +57,7 @@ export class UserRegistrationService {
     return this.http.get(apiUrl + 'movies/genre/:Name', {headers: new HttpHeaders(
       {
         Authorization: 'Bearer ' + token,
-      })}).pipe(
-      map(this.extractResponseData),
-      catchError(this.handleError)
-    );
+      })})
   }
 
   getUser(): Observable<any> {
@@ -88,11 +71,7 @@ export class UserRegistrationService {
     return this.http.get(apiUrl + 'users/' + username, {headers: new HttpHeaders(
       {
         Authorization: 'Bearer ' + token,
-      })}).pipe(
-      map(this.extractResponseData),
-      //map((data) => data.FavoriteMovies),
-      catchError(this.handleError)
-    );
+      })})
   }
 
   addFavouriteMovies( movie: any ): Observable<any> {
@@ -103,10 +82,7 @@ export class UserRegistrationService {
     return this.http.post(apiUrl + 'users/' + user.Username + '/movies/' + movie._id, null, {headers: new HttpHeaders(
       {
         Authorization: 'Bearer ' + token,
-      })}).pipe(
-      map(this.extractResponseData),
-      catchError(this.handleError)
-    );
+      })})
   }
 
   editUser(userDetails: any): Observable<any> {
@@ -115,10 +91,7 @@ export class UserRegistrationService {
     return this.http.put(apiUrl + 'users/' + userDetails.Username, userDetails, {headers: new HttpHeaders(
       {
         Authorization: 'Bearer ' + token,
-      })}).pipe(
-      map(this.extractResponseData),
-      catchError(this.handleError)
-    );
+      })})
   }
 
   deleteUser(): Observable<any> {
@@ -127,10 +100,7 @@ export class UserRegistrationService {
     return this.http.delete(apiUrl + 'users/' + user.Username, {headers: new HttpHeaders(
       {
         Authorization: 'Bearer ' + token,
-      })}).pipe(
-      map(this.extractResponseData),
-      catchError(this.handleError)
-    );
+      })})
   }
 
   deleteFavouriteMovies(movie: any): Observable<any> {
@@ -140,10 +110,7 @@ export class UserRegistrationService {
     return this.http.delete(apiUrl + 'users/' + user.Username + '/movies/' + movie._id, {headers: new HttpHeaders(
       {
         Authorization: 'Bearer ' + token,
-      })}).pipe(
-      map(this.extractResponseData),
-      catchError(this.handleError)
-    );
+      })})
   }
 
 private handleError(error: HttpErrorResponse): any {
@@ -158,5 +125,4 @@ private handleError(error: HttpErrorResponse): any {
     'Something bad happened; please try again later.');
   }
 }
-
 
